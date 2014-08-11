d3085a07c50aeaffda04bef408bd1613fba86d72
Sidhu <sidharth.v.mtr@gmail.com>
2014-08-11 18:36:25 +0530

    Fixing bugs

--- app/src/main/java/com/pixel/sidhu/list/MyActivity.java
+++ app/src/main/java/com/pixel/sidhu/list/MyActivity.java
@@ -6,12 +6,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,19 +16,16 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.pixel.sidhu.list.Details;
 import java.lang.CharSequence;
 import java.lang.Object;
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("ResourceType")
-public class MyActivity
-        extends Activity {
+public class MyActivity extends Activity {
+
     List<Details> Details = new ArrayList();
     ListView DetailslistView;
     EditText classtxt;
@@ -39,36 +33,46 @@ public class MyActivity
     EditText markstxt;
     EditText nametxt;
 
-    public void addDetails(String string, String string2, String string3, String string4) {
-        this.Details.add((Object)(new Details(string, string2, string3, string4)));
+    public void (String string,String string2, String string3, String string4) {
+        this.Details.add(new Details(string, string2, string3, string4));
     }
 
+    @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        this.setContentView(2130903040);
-        this.nametxt = (EditText)(this.findViewById(2131230722));
-        this.dobtxt = (EditText)(this.findViewById(2131230723));
-        this.classtxt = (EditText)(this.findViewById(2131230724));
-        this.markstxt = (EditText)(this.findViewById(2131230725));
-        this.DetailslistView = (ListView)(this.findViewById(2131230729));
-        TabHost tabHost = (TabHost)(this.findViewById(2131230720));
-        tabHost.setup();
-        TabHost.TabSpec tabSpec = tabHost.newTabSpec("Creator");
-        tabSpec.setContent(2131230721);
-        tabSpec.setIndicator((CharSequence)("Creator"));
-        tabHost.addTab(tabSpec);
-        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("List");
-        tabSpec2.setContent(2131230727);
-        tabSpec2.setIndicator((CharSequence)("List"));
-        tabHost.addTab(tabSpec2);
-        TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("About us");
-        tabSpec3.setContent(2131230730);
-        tabSpec3.setIndicator((CharSequence)("About us"));
-        tabHost.addTab(tabSpec3);
-        Button button = (Button)(this.findViewById(2131230726));
-        button.setOnClickListener((View.OnClickListener)(new View.OnClickListener(){
 
-            public void onClick(View view) {
+        setContentView(MyActivity);
+
+        nametxt = (EditText)(findViewById(R.id.textstud_name));
+        dobtxt = (EditText)(findViewById(R.id.textdob));
+        classtxt = (EditText)(findViewById(R.id.textClass));
+        markstxt = (EditText)(findViewById(R.id.textmarks));
+        DetailslistView = (ListView)(findViewById(R.id.listView));
+        TabHost tabHost = (TabHost)(findViewById(R.id.tabHost));
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("Creator");
+        tabSpec.setContent(R.id.tabcreator);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("List");
+        tabSpec.setContent(R.id.tablist);
+        tabSpec.setIndicator("List");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("About us");
+        tabSpec.setContent(R.id.tababtus);
+        tabSpec.setIndicator("About us");
+        tabHost.addTab(tabSpec);
+
+        final Button button = (Button)(findViewById(R.id.btnadd));
+                @Override
+                public void onClick(View View) {
+
+
+        button.setOnClickListener(new View.OnClickListener(){                           //(error) will be looked up later.
                 MyActivity.this.addDetails(MyActivity.this.nametxt.getText().toString(), MyActivity.this.dobtxt.getText().toString(), MyActivity.this.classtxt.getText().toString(), MyActivity.this.markstxt.getText().toString());
                 MyActivity.this.populatelist();
                 Toast.makeText((Context)(MyActivity.this.getApplicationContext()), (CharSequence)("Your details has been added to list!"), (int)(0)).show();
@@ -104,24 +108,24 @@ public class MyActivity
 
     public void populatelist() {
         DetailslistAdapter detailslistAdapter = new DetailslistAdapter();
-        this.DetailslistView.setAdapter((ListAdapter)(detailslistAdapter));
+        this.DetailslistView.setAdapter(detailslistAdapter);
     }
 
     public class DetailslistAdapter
             extends ArrayAdapter<Details> {
         public DetailslistAdapter() {
-            super((Context)(MyActivity.this), 2130903041, MyActivity.this.Details);
+            super(MyActivity.this, DetailslistAdapter, MyActivity.this.Details);
         }
 
         public View getView(int n, View view, ViewGroup viewGroup) {
             if (view == null) {
-                view = MyActivity.this.getLayoutInflater().inflate(2130903041, viewGroup, false);
+                view = MyActivity.this.getLayoutInflater().inflate(DetailslistView, viewGroup, false);
             }
-            Details details = (Details)(MyActivity.this.Details.get(n));
-            (TextView)(view.findViewById(2131230734)).setText((CharSequence)(details.getName()));
-            (TextView)(view.findViewById(2131230735)).setText((CharSequence)(details.getDob()));
-            (TextView)(view.findViewById(2131230736)).setText((CharSequence)(details.getClas()));
-            (TextView)(view.findViewById(2131230737)).setText((CharSequence)(details.getMarks()));
+            Details details = (MyActivity.this.Details.get(n));
+            (TextView)(view.findViewById(R.id.textView)).setText.details.getName();
+            (TextView)(view.findViewById(R.id.textView)).setText.details.getDob();
+            (TextView)(view.findViewById(R.id.textView)).setText.details.getClas();
+            (TextView)(view.findViewById(R.id.textView)).setText.details.getMarks();
             return view;
         }
     }
